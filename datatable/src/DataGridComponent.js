3c9eee30f5d727675e8836c7d98af389d9041835
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2024-04-24 11:15:29 +0500

    delete implemented

--- datatable/src/DataGridComponent.js
+++ datatable/src/DataGridComponent.js
@@ -5,26 +5,26 @@ import dayjs from "dayjs";
 import "./TableComponent.css";
 // import Clock from "react-live-clock";
 
-const rows = [
-    { name: "Elizabeth", start: 1565, end: 1603, id: 0 },
-    { name: "James I", start: 1603, end: 1625, id: 1 },
-    { name: "Charles I", start: 1625, end: 1649, id: 2 },
-    {
-        name: "Cromwell",
-        start: 1649,
-        end: 1660,
-        commonwealth: true,
-        id: 3,
-    },
-    { name: "Charles II", start: 1660, end: 1685, id: 4 },
-    { name: "James II", start: 1685, end: 1689, id: 5 },
-    { name: "W&M", start: 1689, end: 1702, id: 6 },
-    { name: "Anne", start: 1702, end: 1714, id: 7 },
-    { name: "George I", start: 1714, end: 1727, id: 8 },
-    { name: "George II", start: 1727, end: 1760, id: 9 },
-    { name: "George III", start: 1760, end: 1820, id: 10 },
-    { name: "George IV", start: 1820, end: 1820, id: 11 },
-];
+// const rows = [
+//     { name: "Elizabeth", start: 1565, end: 1603, id: 0 },
+//     { name: "James I", start: 1603, end: 1625, id: 1 },
+//     { name: "Charles I", start: 1625, end: 1649, id: 2 },
+//     {
+//         name: "Cromwell",
+//         start: 1649,
+//         end: 1660,
+//         commonwealth: true,
+//         id: 3,
+//     },
+//     { name: "Charles II", start: 1660, end: 1685, id: 4 },
+//     { name: "James II", start: 1685, end: 1689, id: 5 },
+//     { name: "W&M", start: 1689, end: 1702, id: 6 },
+//     { name: "Anne", start: 1702, end: 1714, id: 7 },
+//     { name: "George I", start: 1714, end: 1727, id: 8 },
+//     { name: "George II", start: 1727, end: 1760, id: 9 },
+//     { name: "George III", start: 1760, end: 1820, id: 10 },
+//     { name: "George IV", start: 1820, end: 1820, id: 11 },
+// ];
 
 const sendEdit = (params) => {
     console.log("Edit function");
@@ -34,10 +34,10 @@ const sendEdit = (params) => {
 const sendDelete = (params) => {
     console.log(params);
     axios
-        .delete("http://localhost:8000/api/medicines", { data: params })
+        .delete(`http://localhost:8000/api/medicines/${params.id}`)//, { data: params })
         .then((response) => {
             console.log(response);
-            alert(response.data.message);
+            // alert(response.data.message);
         })
         .catch((error) => {
             console.log(error);
@@ -78,7 +78,7 @@ const columns = [
                 <button
                     className="tableButton"
                     onClick={() => {
-                        alert("I am clicked to delete the selected row");
+                        // alert("I am clicked to delete the selected row");
                         console.log(params.row);
                         sendDelete(params.row);
                     }}
